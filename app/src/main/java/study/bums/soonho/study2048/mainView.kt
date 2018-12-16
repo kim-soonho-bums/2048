a470ca801b5e306a6329e6d382defbe1c2d17906
KIM BUMNYEONG <kimjoin2@gmail.com>
2018-12-16 14:02:29 +0900

    First commit

---
+++ app/src/main/java/study/bums/soonho/study2048/mainView.kt
@@ -0,0 +1,12 @@
+package study.bums.soonho.study2048
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class mainView : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_view)
+    }
+}
