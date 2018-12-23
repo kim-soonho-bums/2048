57fc5c43f16abbc0c4c209b6e10e16da294202de
KIM BUMNYEONG <kimjoin2@gmail.com>
2018-12-23 14:10:08 +0900

    @@

--- app/src/main/java/study/bums/soonho/study2048/MainView.kt
+++ app/src/main/java/study/bums/soonho/study2048/MainView.kt
@@ -2,11 +2,20 @@ package study.bums.soonho.study2048
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.content.Intent
 
-class mainView : AppCompatActivity() {
+
+
+class MainView : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_view)
+    }
+
+    fun startButtonClicked(view: View) {
+        val intent = Intent(this, GameMain::class.java)
+        startActivity(intent)
     }
 }
